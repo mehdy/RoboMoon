6f1abca40abf99c28dd61685895b0d8e8d667d2d
mehdy <mehdy.loveu@hotmail.com>
2014-03-07 00:12:33 +0330

    added some other pages to site

--- app/views.py
+++ app/views.py
@@ -6,6 +6,18 @@ from app import app
 def index():
 	return render_template('index.html')
 
+@app.route('/news/')
+def news():
+	return render_template('news.html')
+
+@app.route('/sessions/')
+def sessions():
+	return render_template('sessions.html')
+
+@app.route('/codes/')
+def codes():
+	return render_template('codes.html')
+
 @app.route('/about/')
 def about():
 	return render_template('about.html')

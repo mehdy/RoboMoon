ea4aa32103715817d0314ddb698ea6d76d691261
mehdy <mehdy.loveu@hotmail.com>
2014-04-26 10:46:21 +0430

    added some other niew feature

--- app/forms.py
+++ app/forms.py
@@ -4,15 +4,22 @@ from wtforms.validators import Required, Length, email, url
 import os
 
 class LoginForm(Form):
-    email = TextField('email', validators = [Required(), Length(min = 4, max = 50), email])
+    email = TextField('email', validators = [Required(), Length(min = 4, max = 100), email])
     password = PasswordField('password', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
 
 class AddSessionForm(Form):
 	title = TextField('title', validators = [Required(), Length(min = 5, max = 100)])
-	description = TextAreaField('description', validators = [Length(min = 0, max = 4000)])
+	description = TextAreaField('description', validators = [Length(max = 4000)])
 
 class AddNewsForm(Form):
 	title = TextField('title', validators = [Required(), Length(min = 5, max= 100)])
-	description = TextAreaField('description', validators = [Length(min = 0, max = 4000)])
+	description = TextAreaField('description', validators = [Length(max = 4000)])
 	
+class EditProfileForm(Form):
+	username = TextField('username', validators = [Required(), Length(max = 50)])
+	password = PasswordField('password', validators = [Required(), Length(min =8, max = 64)])
+	confirm = PasswordField('confirm', validators = [Required(), Length(min =8, max = 64)])
+	email = TextField('email', validators = [Required(), Length(min = 4, max = 100), email])
+	website = TextField('website', validators = [Length(max = 50)])
+	bio = description = TextAreaField('bio', validators = [Length(max = 256)])
--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from flask.ext.login import login_user, logout_user, current_user, login_required
+from forms import LoginForm, AddSessionForm, AddNewsForm, EditProfileForm
 from flask import render_template, redirect, url_for, request, g
 from models import User, Session, News
+from datetime import datetime
 from app import app, db, lm
-from forms import LoginForm, AddSessionForm, AddNewsForm
 from hashlib import sha256
 
 #================Login & Logout==================#
@@ -68,7 +69,9 @@ def codes():
 @app.route('/about/')
 @app.route('/about/<username>/')
 def about(username = None):
-	return render_template('about.html', title = 'about', ab = 'active')
+	user = User.query.filter_by(username = username).first()
+	users = User.query.all()
+	return render_template('about.html', title = 'about',users = users, user = user, ab = 'active')
 
 #=====================Admin section========================#
 @app.route('/admin/')
@@ -101,11 +104,11 @@ def admin_sessions(id = None):
 			title = form.title.data,
 			description = form.description.data,
 			user_id = g.user.id,
-			created = datetime.now())
+			modified = datetime.now())
 		db.session.add(session)
 		db.session.commit()
 	sessions = Session.query.all()
-	return render_template('admin_sessions.html', sessions = sessions, id = id, title = 'Sessions Records', se = 'active')
+	return render_template('admin_sessions.html', form = form, sessions = sessions, id = id, title = 'Sessions Records', se = 'active')
 
 @app.route('/admin/news/', methods = ['GET', 'POST'])
 @app.route('/admin/news/<id>', methods = ['GET', 'POST', 'DELETE'])
@@ -132,7 +135,7 @@ def admin_news(id = None):
 			title = form.title.data,
 			description = form.description.data,
 			user_id = g.user.id,
-			created = datetime.now())
+			modified = datetime.now())
 		db.session.add(news)
 		db.session.commit()
 	allnews = News.query.all()
@@ -141,9 +144,29 @@ def admin_news(id = None):
 @app.route('/admin/codes/', methods = ['GET', 'POST'])
 @login_required
 def admin_codes():
+	#Here needs to add code
 	return render_template('admin_codes.html', title = 'codes', co = 'active')
 
 @app.route('/admin/profile/', methods = ['GET', 'POST'])
 @login_required
-def admin_users(username = None):
-	return render_template('admin_profile.html', title = 'Profile', po = 'active')
+def admin_users():
+	form = EditProfileForm(request.form)
+	if request.method == 'POST':
+		user = g.user
+		if form.username.data:
+			if not User.query.filter_by(username = form.username.data).first():
+				user.username = form.username.data
+		if form.email.data:
+			if not User.query.filter_by(email = form.email.data).first():
+				user.email = form.email.data
+		if form.password.data and form.confirm.data and form.password.data == form.confirm.data:
+			print form.password.data
+			user.password = sha256(form.password.data).hexdigest()
+		if form.website.data:
+			user.website = form.website.data
+		if form.bio.data:
+			user.bio = form.bio.data
+		db.session.add(user)
+		db.session.commit()
+		#Do the verfication and change the data
+	return render_template('admin_profile.html', form = form, user = g.user, title = 'Profile', po = 'active')
